f53504f055cb7f2733a00163e85279da5adc5d6f
Rinier Dapugo <rinierdapugo@gmail.com>
2023-03-18 14:38:11 +0800

    UPDATED

--- src/routers/movie-router.tsx
+++ src/routers/movie-router.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Detail from '../pages/Details';
+import DowinnLogin from '../pages/dowinn-login';
 import Home from '../pages/Home';
 
 function MovieRoute() {
   return (
       <Routes>
+          <Route path="/dowin" element={<DowinnLogin />} />
           <Route path="/movie/:id" element={<Detail />} />
           <Route path="/" element={<Home />} />
       </Routes>
